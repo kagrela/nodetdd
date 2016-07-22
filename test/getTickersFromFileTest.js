d9c1d7d834fb2bc6ad8ebcfb839d104584fe7f49
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-07-22 11:36:01 +0200

    getTickersFromFile promise based test

--- test/getTickersFromFileTest.js
+++ test/getTickersFromFileTest.js
@@ -3,12 +3,20 @@ const mocha = require('mocha');
 
 describe('getTickersFromFile', function () {
     it('should extract tickers from file', function () {
-        const readFile = function(fileName) {};
-        const extractTickers = function(fileContent) {};
+        const readFile = function(fileName) {
+            assert.equal(fileName, 'someFile');
+            return Promise.resolve('file content');
+        };
+        const extractTickers = function(fileContent) {
+            assert.equal(fileContent, 'file content');
+            return ['A', 'B', 'C'];
+        };
 
         const getTickersFromFile = require('../lib/getTickersFromFile')({readFile, extractTickers});
 
-        return getTickersFromFile('./symbolsFile');
+        return getTickersFromFile('someFile').then(function(tickers) {
+            assert.deepEqual(tickers, ['A', 'B', 'C']);
+        });
     });
 
 });
