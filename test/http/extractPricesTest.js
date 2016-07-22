7fddea8197fcccf74beec5b8bff7983351476235
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-07-22 15:30:10 +0200

    string formatting

--- test/http/extractPricesTest.js
+++ test/http/extractPricesTest.js
@@ -4,7 +4,9 @@ const extractPrices = require('../../lib/http/extractPrices');
 describe('extract prices', function () {
 
     it('should translate responses to current prices', function () {
-        var data = 'Date,Open,High,Low,Close,Volume,Adj Close\n\ 2015-09-11,619.75,625.780029,617.419983,625.77002,1360900,625.77002\n\ 2015-09-10,613.099976,624.159973,611.429993,621.349976,1900500,621.349976';
+        var data = 'Date,Open,High,Low,Close,Volume,Adj Close\n\ ' +
+            '2015-09-11,619.75,625.780029,617.419983,625.77002,1360900,625.77002\n\ ' +
+            '2015-09-10,613.099976,624.159973,611.429993,621.349976,1900500,621.349976';
 
         var response = {
             statusCode: 200,
