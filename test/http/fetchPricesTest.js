026e41788c8b04142eb1284029c2726f2e260ae3
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2016-07-22 15:10:18 +0200

    fetch prices test

---
+++ test/http/fetchPricesTest.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const fetchPrices = require('../../lib/http/fetchPrices');
+
+describe('fetch prices', function () {
+    it.skip('[integration test] should fetch a symbol', function *() {
+        const request = require('good-guy-http')();
+        const fetch = fetchPrices({request});
+
+        const results = yield fetch(['AAPL', 'GOOG', 'INVALID']);
+
+        assert.equal(results[0].statusCode, 200);
+        assert.equal(results[1].statusCode, 200);
+        assert.equal(results[2].statusCode, 404);
+        assert.equal(results.length, 3);
+    });
+
+    it('[unit test] should fetch a symbol', function *() {
+        const request = function (url) {
+            return url.endsWith('INVALID') ? Promise.reject({statusCode: 404}) : Promise.resolve({statusCode: 200});
+        };
+        const fetch = fetchPrices({request});
+
+        const results = yield fetch(['AAPL', 'GOOG', 'INVALID']);
+
+        assert.equal(results[0].statusCode, 200);
+        assert.equal(results[1].statusCode, 200);
+        assert.equal(results[2].statusCode, 404);
+        assert.equal(results.length, 3);
+    });
+
+});
