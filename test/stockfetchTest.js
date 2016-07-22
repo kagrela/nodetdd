eb6e81718762e105a4b40091ed120c614de70754
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-07-22 10:39:26 +0200

    should call get tickers from file

--- test/stockfetchTest.js
+++ test/stockfetchTest.js
@@ -3,7 +3,13 @@ const mocha = require('mocha');
 
 describe('stockfetch', function () {
     it('happypath', function () {
-        const stockfetch = require('../lib/stockfetch')();
+        const getTickersFromFile = function(file) {
+            assert.equal(file, './symbolsFile');
+        };
+
+        const stockfetch = require('../lib/stockfetch')({getTickersFromFile});
+
+        stockfetch('./symbolsFile');
     });
 
 });
